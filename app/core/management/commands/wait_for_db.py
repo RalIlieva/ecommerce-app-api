1a0bcdf41345b3515b50de636a65f0814545fb4d
RalIlieva <rilieva8820@gmail.com>
2024-07-31 15:59:47 +0300

    Add test for wait_for_db and implement the feature

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -18,7 +18,7 @@ class Command(BaseCommand):
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except(Psycopg2Error, OperationalError):
+            except (Psycopg2Error, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
