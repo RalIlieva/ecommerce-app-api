d62435f36749215ffdd44a0ad6dfe2d10d448905
RalIlieva <rilieva8820@gmail.com>
2024-08-02 17:36:04 +0300

    Add test for too short password for user API and implement the feature

--- app/users/tests/test_api_users.py
+++ app/users/tests/test_api_users.py
@@ -48,3 +48,17 @@ class PublicUserApiTests(TestCase):
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short_error(self):
+        """Test an error is returned if password less than 5 chars."""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'pw',
+            'name': 'Test Name',
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
