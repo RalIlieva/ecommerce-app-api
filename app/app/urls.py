94108b9897fc194edf12aa0c40e81fb3881e6f6a
RalIlieva <rilieva8820@gmail.com>
2024-08-06 20:58:11 +0300

    Rename api/token/ to api/login/ in app/urls.py - to be explicit

--- app/app/urls.py
+++ app/app/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
          name='api-docs',
          ),
     path('api/user/', include('users.urls')),
-    path('api/token/', TokenObtainPairView.as_view(),
+    path('api/login/', TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(),
          name='token_refresh'),
