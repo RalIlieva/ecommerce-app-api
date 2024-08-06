d11471a7dc407f3ebc51e2a83574fdd108a7d8a5
RalIlieva <rilieva8820@gmail.com>
2024-08-06 21:50:09 +0300

    Add TimeStampedModel in core/models

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,9 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
--- app/users/models.py
+++ app/users/models.py
@@ -2,6 +2,7 @@
 Custom user model.
 """
 
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractUser,
@@ -48,3 +49,13 @@ class User(AbstractUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+# class CustomerProfile(models.Model):
+#     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     phone_number = models.CharField(max_length=15, blank=True, null=True)
+#     address = models.CharField(max_length=255, blank=True, null=True)
+#     date_of_birth = models.DateField(blank=True, null=True)
+#
+#     def __str__(self):
+#         return self.user.email
