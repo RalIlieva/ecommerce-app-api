4bcb61792a14245e80c891af128d289888473803
RalIlieva <rilieva8820@gmail.com>
2024-08-07 21:40:56 +0300

    Add test for api customer profile - retrieve successfully

--- app/users/tests/test_api_customer_profile.py
+++ app/users/tests/test_api_customer_profile.py
@@ -38,4 +38,4 @@ class PrivateCustomerProfileApiTests(TestCase):
         res = self.client.get(PROFILE_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        # self.assertEqual(res.data['user'], self.user.id)
+        self.assertEqual(res.data['user'], self.user.id)
