b0b4e5344db475b1315d4c32e6d7c1db8f196e3e
RalIlieva <rilieva8820@gmail.com>
2024-08-12 16:44:53 +0300

    Create models representing product

--- app/app/settings.py
+++ app/app/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'core',
     'users.apps.UsersConfig',
     'administrators.apps.AdministratorsConfig',
+    'products',
     'rest_framework',
     'drf_spectacular',
     'rest_framework_simplejwt',
@@ -126,7 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/static/'
+MEDIA_ULR = '/static/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- app/products/models.py
+++ app/products/models.py
@@ -1,3 +1,80 @@
+"""
+Products model.
+"""
+import uuid
+import os
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampedModel
+
+
+def product_image_file_path(instance, filename):
+    """Generate file path for new product image using UUID."""
+    # Get the file extension
+    ext = os.path.splitext(filename)[1]
+    # Generate a new filename with UUID
+    filename = f'{uuid.uuid4()}{ext}'
+    return os.path.join('uploads', 'product', filename)
+
+
+class Category(TimeStampedModel):
+    """ Category for products."""
+    name = models.CharField(max_length=255)
+    # Prevent deletion of categories to children products.
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+        related_name='children'
+    )
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    """Tags for products."""
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(TimeStampedModel):
+    """Product model."""
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True, related_name='products')
+    stock = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    """Model representing an image associated with a product."""
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to=product_image_file_path)
+    alt_text = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f'Image for {self.product.name}'
+
+
+class Review(TimeStampedModel):
+    """Reviews for users that are customers."""
+    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"Review for {self.product.name} by {self.user.email}"
