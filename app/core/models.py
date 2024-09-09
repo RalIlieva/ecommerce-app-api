81d3c7566488f6ed705c70032d77061d2fe60de2
RalIlieva <rilieva8820@gmail.com>
2024-09-09 14:25:16 +0300

    Add comment to TimeStampedModel in core

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class TimeStampedModel(models.Model):
+    """
+    An abstract base model class that provides self-\
+    updating 'created' and 'modified' fields.
+    """
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
