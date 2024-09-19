f2da552e241b106dc88d5da80f721a1ffed59f4c
RalIlieva <rilieva8820@gmail.com>
2024-09-19 14:43:57 +0300

    Fine-tune products/admin for readability

--- app/products/admin.py
+++ app/products/admin.py
@@ -13,7 +13,28 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['is_active', 'category', 'tags']
 
 
-admin.site.register(Category)
-admin.site.register(Tag)
+# admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'parent', 'slug']
+    list_filter = ['created', 'products']
+
+
+# admin.site.register(Tag)
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    list_filter = ['products']
+
+
 admin.site.register(ProductImage)
-admin.site.register(Review)
+# @admin.register(ProductImage)
+# class ProductImageAdmin(admin.ModelAdmin):
+#     list_display = ['alt_text', 'products']
+
+
+# admin.site.register(Review)
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['product', 'user', 'rating']
+    list_filter = ['rating', 'user', 'product']
