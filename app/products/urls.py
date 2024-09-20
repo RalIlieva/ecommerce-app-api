6230fa0584564bc308adb7b996cfc662f3c50fa2
RalIlieva <rilieva8820@gmail.com>
2024-09-20 20:49:56 +0300

    Refactor and comment product/urls

--- app/products/urls.py
+++ app/products/urls.py
@@ -29,66 +29,60 @@ urlpatterns = [
     path('products/create/',
          ProductCreateView.as_view(),
          name='product-create'
-         ),
+         ),  # Admin
     path('products/<uuid:uuid>/manage/',
          ProductUpdateDeleteView.as_view(),
          name='product-manage'
-         ),
+         ),  # Admin
     path('products/<uuid:uuid>/<slug:slug>/',
          ProductDetailView.as_view(),
          name='product-detail'
-         ),
-    path('products/', ProductListView.as_view(), name='product-list'),
-    # path('products/<int:pk>/manage/',
-    #      ProductUpdateDeleteView.as_view(),
-    #      name='product-manage'
-    #      ),
-    # path('products/<int:pk>/',
-    #      ProductDetailView.as_view(),
-    #      name='product-detail'
-    #      ),
+         ),  # User-facing detail
+    path('products/',
+         ProductListView.as_view(),
+         name='product-list'
+         ),  # User-facing list
 
     # Category URLs
-    path('categories/', CategoryListView.as_view(), name='category-list'),
     path('categories/create/',
          CategoryCreateView.as_view(),
          name='category-create'
-         ),
+         ),  # Admin
     path('categories/<int:pk>/manage/',
          CategoryUpdateDeleteView.as_view(),
          name='category-manage'
-         ),
+         ),  # Admin
     path('categories/<slug:slug>/',
          CategoryDetailView.as_view(),
          name='category-detail'),  # User-facing detail
+    path('categories/',
+         CategoryListView.as_view(),
+         name='category-list'
+         ),  # User-facing list
 
     # Tag URLs
-    path('tags/', TagListView.as_view(), name='tag-list'),
-    path('tags/create/', TagCreateView.as_view(), name='tag-create'),
+    path('tags/create/',
+         TagCreateView.as_view(),
+         name='tag-create'
+         ),  # Admin-facing
     path('tags/<int:pk>/manage/',
          TagUpdateDeleteView.as_view(),
          name='tag-manage'
-         ),
+         ),  # Admin-facing
     path('tags/<slug:slug>/',
          TagDetailView.as_view(),
          name='tag-detail'),  # User-facing detail
+    path('tags/',
+         TagListView.as_view(),
+         name='tag-list'),  # User-facing list
 
     # Product Image URL
-    # path('products/<int:product_id>/upload-image/',
-    #      ProductImageUploadView.as_view(),
-    #      name='product-image-upload'
-    #      ),
-    # path('products/<int:product_id>/images/<int:image_id>/delete/',
-    #      ProductImageDeleteView.as_view(),
-    #      name='product-image-delete'
-    #      ),
-
     path('products/<uuid:uuid>/<slug:slug>/upload-image/',
          ProductImageUploadView.as_view(),
          name='product-image-upload'
-         ),
+         ),  # Admin
     path('products/<uuid:uuid>/<slug:slug>/images/<int:image_id>/delete/',
          ProductImageDeleteView.as_view(),
          name='product-image-delete'
-         ),
+         ),  # Admin
 ]
