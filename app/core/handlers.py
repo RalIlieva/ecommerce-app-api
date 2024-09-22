e2d813e72c34d89afb0552fe06c3a9e93f39d7c0
RalIlieva <rilieva8820@gmail.com>
2024-09-22 17:06:36 +0300

    Clean up handlers

--- app/core/handlers.py
+++ app/core/handlers.py
@@ -21,23 +21,6 @@ from .exceptions import DuplicateSlugException
 logger = logging.getLogger(__name__)
 
 
-# def custom_exception_handler(exc, context):
-#     """
-#     Custom exception handler that logs exceptions.
-#     """
-#     # Call DRF's default exception handler first
-#     response = exception_handler(exc, context)
-#
-#     # If exception is DuplicateSlugException, customize the response
-#     if isinstance(exc, DuplicateSlugException):
-#         logger.warning(f"Duplicate slug error: {exc.detail}")
-#         response = Response(
-#             {"detail": exc.detail},
-#             status=exc.status_code
-#         )
-#
-#     return response
-
 def drf_default_with_modifications_exception_handler(exc, context):
     """
     Custom exception handler that ensures -
