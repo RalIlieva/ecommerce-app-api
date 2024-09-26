96471271fe1917852a29b3c285c7cebc97964896
RalIlieva <rilieva8820@gmail.com>
2024-09-26 19:21:31 +0300

    Implement Category Filter for categories views

--- app/products/views/tags_views.py
+++ app/products/views/tags_views.py
@@ -5,8 +5,8 @@ Views for the products' tags API.
 from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ValidationError
-# from django_filters.rest_framework import DjangoFilterBackend
-# from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework import generics, permissions
 from rest_framework import serializers
 from core.exceptions import DuplicateSlugException
@@ -16,6 +16,8 @@ from ..serializers import (
     TagListSerializer,
     TagDetailSerializer,
 )
+from ..filters import TagFilter
+from ..pagination import CustomPagination
 
 
 # Tag Views
@@ -25,6 +27,10 @@ class TagListView(generics.ListAPIView):
     """
     queryset = Tag.objects.all().order_by('id')
     serializer_class = TagListSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_class = TagFilter
+    search_fields = ['name']  # Fields to search by
+    pagination_class = CustomPagination
     permission_classes = [permissions.AllowAny]
 
 
