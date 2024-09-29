89b9d7577fa97878e34310f24f25ef0e10400fae
RalIlieva <rilieva8820@gmail.com>
2024-09-29 21:23:55 +0300

    Fix flake8

--- app/users/management/commands/fix_duplicate_uuids.py
+++ app/users/management/commands/fix_duplicate_uuids.py
@@ -14,10 +14,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         User = get_user_model()
-        duplicate_uuids = User.objects.values('uuid').annotate(uuid_count=Count('uuid')).filter(uuid_count__gt=1)
+        duplicate_uuids = User.objects.values('uuid').\
+            annotate(uuid_count=Count('uuid')).filter(uuid_count__gt=1)
 
         if not duplicate_uuids.exists():
-            self.stdout.write(self.style.SUCCESS("No duplicate UUIDs found in User model."))
+            self.stdout.write(
+                self.style.SUCCESS("No duplicate UUIDs found in User model.")
+            )
             return
 
         self.stdout.write("Duplicate UUIDs in User model:")
@@ -26,12 +29,18 @@ class Command(BaseCommand):
             count = entry['uuid_count']
             self.stdout.write(f"UUID: {duplicated_uuid} - Count: {count}")
 
-            users_with_duplicate_uuid = User.objects.filter(uuid=duplicated_uuid)
+            users_with_duplicate_uuid = User.objects.filter(
+                uuid=duplicated_uuid
+            )
             # Retain the first user, assign new UUIDs to the rest
             for user in users_with_duplicate_uuid[1:]:
                 new_uuid = uuid.uuid4()
                 user.uuid = new_uuid
                 user.save()
-                self.stdout.write(f"Assigned new UUID {new_uuid} to user {user.email}")
+                self.stdout.write(
+                    f"Assigned new UUID {new_uuid} to user {user.email}"
+                )
 
-        self.stdout.write(self.style.SUCCESS("All duplicate UUIDs have been resolved."))
+        self.stdout.write(
+            self.style.SUCCESS("All duplicate UUIDs have been resolved.")
+        )
