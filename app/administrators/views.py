c08548c449d8a64959ac63740814587e20e76657
RalIlieva <rilieva8820@gmail.com>
2024-10-01 18:54:59 +0300

    Refactor administrators app to look up for uuid instead of id

--- app/administrators/views.py
+++ app/administrators/views.py
@@ -3,6 +3,7 @@ The custom api administrator views.
 """
 
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 
 from rest_framework import mixins, viewsets, permissions
 from users.models import CustomerProfile
@@ -23,6 +24,12 @@ class AdministratorUserViewSet(
     queryset = get_user_model().objects.all().order_by('id')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+    lookup_field = 'uuid'  # Use 'uuid' instead of 'id'
+
+    def get_object(self):
+        """Retrieve and return the user by UUID."""
+        uuid = self.kwargs.get('uuid')
+        return get_object_or_404(get_user_model(), uuid=uuid)
 
 
 class AdministratorCustomerProfileViewSet(
@@ -36,3 +43,9 @@ class AdministratorCustomerProfileViewSet(
     queryset = CustomerProfile.objects.all().order_by('id')
     serializer_class = CustomerProfileSerializer
     permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+    lookup_field = 'uuid'  # Use 'uuid' instead of 'id'
+
+    def get_object(self):
+        """Retrieve and return the customer profile by UUID."""
+        uuid = self.kwargs.get('uuid')
+        return get_object_or_404(CustomerProfile, uuid=uuid)
