26dade305a0b704d74718b022e63fb29cb1c0f2a
RalIlieva <rilieva8820@gmail.com>
2024-10-10 21:37:07 +0300

    Add test for average rating in products

--- app/products/tests/serializers/test_product_serializers.py
+++ app/products/tests/serializers/test_product_serializers.py
@@ -2,13 +2,15 @@
 Test API product serializers.
 """
 
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from products.serializers import (
     ProductDetailSerializer
 )
 from products.models import (
     Category,
-    Product
+    Product,
+    Review,
 )
 
 
@@ -107,3 +109,35 @@ class ProductSerializerTest(TestCase):
             "slug", serializer.errors,
             "Expected 'slug' field error in serializer errors."
         )
+
+
+class ProductDetailSerializerTest(TestCase):
+    def setUp(self):
+        # Create a user model instance
+        self.user_model = get_user_model()
+
+        # Create two users to use as foreign keys for the reviews
+        self.user1 = self.user_model.objects.create_user(
+            email="user1@example.com", password="password123"
+        )
+        self.user2 = self.user_model.objects.create_user(
+            email="user2@example.com", password="password123"
+        )
+        # Create a category and product
+        self.category = Category.objects.create(
+            name="Electronics",
+            slug="electronics"
+        )
+        self.product = Product.objects.create(
+            name="Test Product", description="A great product", price=100.00,
+            category=self.category, stock=10, slug="test-product"
+        )
+        # Create reviews linked to the created users and the product
+        Review.objects.create(product=self.product, user=self.user1, rating=4)
+        Review.objects.create(product=self.product, user=self.user2, rating=5)
+
+    def test_average_rating_in_serializer(self):
+        serializer = ProductDetailSerializer(self.product)
+        data = serializer.data
+        self.assertIn('average_rating', data)
+        self.assertAlmostEqual(data['average_rating'], 4.5, places=1)
