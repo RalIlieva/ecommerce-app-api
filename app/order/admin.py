803fca8ceed10bf373dc631800f75a4541373a8a
RalIlieva <rilieva8820@gmail.com>
2024-10-11 19:22:39 +0300

    Register order in admin

--- app/order/admin.py
+++ app/order/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+""""
+Register order models.
+"""
 
-# Register your models here.
+
+from django.contrib import admin
+from .models import Order
+
+admin.site.register(Order)
