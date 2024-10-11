4126255f223d005acb2b5ad776788892beb5131f
RalIlieva <rilieva8820@gmail.com>
2024-10-11 19:40:28 +0300

    Add order selectors

---
+++ app/order/selectors.py
@@ -0,0 +1,13 @@
+"""
+Business logic - fetching from db.
+"""
+
+from .models import Order
+
+
+def get_user_orders(user):
+    return Order.objects.filter(user=user).order_by('-created')
+
+
+def get_order_details(order_id):
+    return Order.objects.prefetch_related('order_items__product').get(id=order_id)
