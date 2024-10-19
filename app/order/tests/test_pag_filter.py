fbfcb8305a74c9dc00d19e037c41723feccee925
RalIlieva <rilieva8820@gmail.com>
2024-10-19 21:12:09 +0300

    Fix flake8

--- app/order/tests/test_pag_filter.py
+++ app/order/tests/test_pag_filter.py
@@ -1,12 +1,7 @@
-from django.contrib.auth import get_user_model
-from django.test import TestCase
 from django.urls import reverse
-from rest_framework.test import APIClient
 from rest_framework import status
-from products.models import Category, Product
 from order.models import Order
 from order.services import create_order
-from uuid import uuid4
 from .test_base import OrderTestBase
 
 ORDERS_URL = reverse('order:order-list')
@@ -14,10 +9,14 @@ ORDER_CREATE_URL = reverse('order:order-create')
 
 
 class OrderCreationTestCase(OrderTestBase):
-    """Test suite for creating orders using Order Create View."""
+    """
+    Test suite for creating orders using Order Create View.
+    """
 
     def test_create_order_with_valid_data(self):
-        """Test creating an order with valid data."""
+        """
+        Test creating an order with valid data.
+        """
         payload = {
             'items': [{'product': str(self.product.uuid), 'quantity': 2}]
         }
@@ -27,10 +26,15 @@ class OrderCreationTestCase(OrderTestBase):
         self.assertEqual(Order.objects.count(), 1)
 
     def test_list_orders_pagination(self):
-        """Test that the order list API supports pagination."""
+        """
+        Test that the order list API supports pagination.
+        """
         # Create several orders
         for _ in range(15):
-            self.create_order(self.user, [{'product': self.product.uuid, 'quantity': 1}])
+            self.create_order(
+                self.user,
+                [{'product': self.product.uuid, 'quantity': 1}]
+            )
 
         response = self.client.get(ORDERS_URL + '?page=1&page_size=10')
 
@@ -39,7 +43,10 @@ class OrderCreationTestCase(OrderTestBase):
 
     def test_list_orders_filter_status(self):
         """Test filtering orders by status."""
-        shipped_order = create_order(self.user, [{'product': self.product.uuid, 'quantity': 1}])
+        shipped_order = create_order(
+            self.user,
+            [{'product': self.product.uuid, 'quantity': 1}]
+        )
         shipped_order.status = Order.SHIPPED
         shipped_order.save()
 
@@ -47,4 +54,7 @@ class OrderCreationTestCase(OrderTestBase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['results']), 1)
-        self.assertEqual(response.data['results'][0]['uuid'], str(shipped_order.uuid))
+        self.assertEqual(
+            response.data['results'][0]['uuid'],
+            str(shipped_order.uuid)
+        )
