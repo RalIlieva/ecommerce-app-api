bff7e6fc733599bf6d881c262a88216b82de366e
RalIlieva <rilieva8820@gmail.com>
2024-10-22 18:15:03 +0300

    Add stripe API Keys in settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -221,3 +221,7 @@ LOGGING = {
         # TO DO - define other loggers as needed
     },
 }
+
+# Stripe Settings
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET')
