1428295a6d1b1b0e5bf577f09ce2558f2f60671a
RalIlieva <rilieva8820@gmail.com>
2024-10-23 00:20:33 +0300

    Add payment selectors

--- app/payment/selectors.py
+++ app/payment/selectors.py
@@ -1,9 +1,18 @@
-# from .models import Payment
-#
-#
-# def get_payment_details(transaction_id):
-#     return Payment.objects.get(transaction_id=transaction_id)
-#
-#
-# def get_user_payments(user):
-#     return Payment.objects.filter(order__user=user)
+from .models import Payment
+
+
+def get_user_payments(user):
+    """
+    Retrieve all payments for a specific user.
+    """
+    return Payment.objects.filter(user=user).order_by('-created')
+
+
+def get_payment_by_uuid(user, uuid):
+    """
+    Retrieve a specific payment by UUID for a user.
+    """
+    try:
+        return Payment.objects.get(user=user, uuid=uuid)
+    except Payment.DoesNotExist:
+        return None
