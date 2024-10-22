f66a70d572d94af10c38130929ce8f67bac5b374
RalIlieva <rilieva8820@gmail.com>
2024-10-22 18:45:00 +0300

    Update .env and docker-compose

--- .env.py
+++
@@ -1,9 +0,0 @@
-# Stripe Keys
-STRIPE_SECRET_KEY=sk_test_51QCRzCFopY0kiYaanYJvwuYNpbmIpl0QjNlMPfxE8PNYD5hNatggNifpd0V0uZbQwlmQ0WZ6V8gFPNt1EwQ6Okv300OshuSusy
-STRIPE_PUBLISHABLE_KEY=pk_test_51QCRzCFopY0kiYaakR4B5pDCLOoeJPG4vsaA9jENSROVAxor1eM3qky0koQMbuZBTHZc2YuFw91hPpW5bNZ6TauR00GHqzUVEr
-
-# Database Credentials
-DB_HOST=db
-DB_NAME=devdb
-DB_USER=devuser
-DB_PASS=changeme
