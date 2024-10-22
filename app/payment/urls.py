60e366f034eff988ce66cf95dc7f221eef82c360
RalIlieva <rilieva8820@gmail.com>
2024-10-23 00:57:25 +0300

    Add urls patterns for PaymentDetailView and PaymentListView

--- app/payment/urls.py
+++ app/payment/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import CreatePaymentView, stripe_webhook
+from .views import CreatePaymentView, stripe_webhook, PaymentDetailView, PaymentListView
+
+app_name = 'payment'
 
 urlpatterns = [
     path('create-payment/', CreatePaymentView.as_view(), name='create-payment'),
     path('webhook/', stripe_webhook, name='stripe-webhook'),
+    path('<uuid:uuid>/', PaymentDetailView.as_view(), name='payment-detail'),
+    path('', PaymentListView.as_view(), name='payment-list'),
 ]
