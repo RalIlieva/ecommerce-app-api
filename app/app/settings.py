f160e41504d9cf630d9860943abf611f577eafc5
RalIlieva <rilieva8820@gmail.com>
2024-10-22 19:01:37 +0300

    Move Django Secret Key in .env

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,7 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@%9=ehqd-yr@f#@$k+ugr%ppnbc2)m9_e=#-#kka(o(mz-fzn%'
+# SECRET_KEY = 'django-insecure-@%9=ehqd-yr@f#@$k+ugr%ppnbc2)m9_e=#-#kka(o(mz-fzn%'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY not found in environment variables.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
