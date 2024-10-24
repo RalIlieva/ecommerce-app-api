011bf5eeec9ab6c84f9dcf68edc62f2a175f67e4
RalIlieva <rilieva8820@gmail.com>
2024-10-24 13:00:20 +0300

    Register payment in admin.py

--- app/payment/admin.py
+++ app/payment/admin.py
@@ -1,3 +1,9 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Payment
 
-# Register your models here.
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['order', 'user', 'amount', 'status', 'stripe_payment_intent_id', 'created', 'modified']
+    list_filter = ['status', 'created', 'modified']
+    search_fields = ['order__id', 'user__email', 'stripe_payment_intent_id']
