0b9c6c8624bab1fb9f60a7c04c4fc2a12aeae789
RalIlieva <rilieva8820@gmail.com>
2024-10-25 17:18:08 +0300

    Clean up code

--- app/cart/selectors.py
+++ app/cart/selectors.py
@@ -9,16 +9,3 @@ def get_cart_total(user):
     cart = get_user_cart(user)
     total = sum(item.product.price * item.quantity for item in cart.items.all())
     return total
-
-
-# from .models import Cart
-#
-#
-# def get_cart(user):
-#     return Cart.objects.get(user=user)
-#
-#
-# def calculate_cart_total(cart):
-#     return sum(
-#     item.product.price * item.quantity for item in cart.items.all()
-#     )
