fda49f979bfb4c0a8c4e7c8458964dbfb340d98c
RalIlieva <rilieva8820@gmail.com>
2024-10-25 14:55:27 +0300

    Implement cart app and cart models - makemigrations and migrate

--- app/cart/models.py
+++ app/cart/models.py
@@ -1,27 +1,35 @@
-# from django.db import models
-# from django.conf import settings
-# from products.models import Product
-#
-# from core.models import (
-#     TimeStampedModel,
-#     UUIDModel
-# )
-#
-#
-# class Cart(TimeStampedModel, UUIDModel):
-#     user = models.OneToOneField(
-#     settings.AUTH_USER_MODEL,
-#     on_delete=models.CASCADE
-#     )
-#
-#
-# class CartItem(models.Model):
-#     cart = models.ForeignKey(
-#     Cart, related_name='items',
-#     on_delete=models.CASCADE
-#     )
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#     quantity = models.PositiveIntegerField()
-#
-#     def __str__(self):
-#         return f"{self.product.name} - {self.quantity} pcs"
+from django.db import models
+from django.conf import settings
+from products.models import Product
+from core.models import UUIDModel, TimeStampedModel
+
+
+class Cart(UUIDModel, TimeStampedModel):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='cart'
+    )
+
+    def __str__(self):
+        return f"Cart for {self.user.email}"
+
+
+class CartItem(UUIDModel, TimeStampedModel):
+    cart = models.ForeignKey(
+        Cart,
+        on_delete=models.CASCADE,
+        related_name='items'
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.PROTECT,
+        related_name='cart_items'
+    )
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in cart {self.cart.id}"
+
+    class Meta:
+        unique_together = ('cart', 'product')  # Each product should be unique per cart
