3e331f25a40b4baf5446f183964e2ec817ec049c
RalIlieva <rilieva8820@gmail.com>
2024-10-25 17:25:08 +0300

    Register cart app in admin

--- app/cart/admin.py
+++ app/cart/admin.py
@@ -1,3 +1,16 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created', 'modified')
+    search_fields = ('user__email',)
+    readonly_fields = ('uuid', 'created', 'modified')
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product', 'quantity', 'created', 'modified')
+    search_fields = ('cart__user__email', 'product__name')
+    readonly_fields = ('uuid', 'created', 'modified')
