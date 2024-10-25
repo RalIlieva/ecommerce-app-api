8110d0a22f189857f00df116d9319852d85e890a
RalIlieva <rilieva8820@gmail.com>
2024-10-25 15:12:56 +0300

    Add cart selectors

--- app/cart/selectors.py
+++ app/cart/selectors.py
@@ -1,3 +1,16 @@
+from .models import Cart
+
+
+def get_user_cart(user):
+    return Cart.objects.prefetch_related('items__product').get(user=user)
+
+
+def get_cart_total(user):
+    cart = get_user_cart(user)
+    total = sum(item.product.price * item.quantity for item in cart.items.all())
+    return total
+
+
 # from .models import Cart
 #
 #
