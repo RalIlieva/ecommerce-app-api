c5a3699b05bbb4c5f77f9072bffb5187547102b7
RalIlieva <rilieva8820@gmail.com>
2024-10-30 14:26:54 +0200

    Register checkout models in admin.py

--- app/checkout/admin.py
+++ app/checkout/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin
+""""
+Register checkout models.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import CheckoutSession
+
+# admin.site.register(CheckoutSession)
+
+
+@admin.register(CheckoutSession)
+class CheckoutSessionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user',
+        'status',
+        'shipping_address',
+        'created', 'modified'
+    )
+    list_filter = ('status', 'created', 'modified')
+    search_fields = ('user__email', 'status', 'shipping_address')
