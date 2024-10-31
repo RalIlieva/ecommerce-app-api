1a125bb6b156069fa376f8f9c4fc2479980d0767
RalIlieva <rilieva8820@gmail.com>
2024-10-31 21:59:40 +0200

    Add wishlist urls

---
+++ app/wishlist/urls.py
@@ -0,0 +1,14 @@
+# from django.urls import path
+# from .views import (
+#     WishlistView,
+#     AddToWishlistView,
+#     RemoveFromWishlistView,
+#     MoveToCartView
+# )
+#
+# urlpatterns = [
+#     path('', WishlistView.as_view(), name='wishlist-detail'),
+#     path('add/', AddToWishlistView.as_view(), name='wishlist-add'),
+#     path('remove/<uuid:product_uuid>/', RemoveFromWishlistView.as_view(), name='wishlist-remove'),
+#     path('move-to-cart/', MoveToCartView.as_view(), name='wishlist-move-to-cart'),
+# ]
