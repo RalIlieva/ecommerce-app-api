75780e00169a70e2920e22efdebcfef7a5a8184b
RalIlieva <rilieva8820@gmail.com>
2024-10-31 22:14:52 +0200

    Wishlist models - makemigrations and migrate

--- app/wishlist/models.py
+++ app/wishlist/models.py
@@ -1,35 +1,34 @@
-# from django.db import models
-# from django.conf import settings
-# from core.models import UUIDModel, TimeStampedModel
-# from products.models import Product
-#
-#
-# class Wishlist(UUIDModel, TimeStampedModel):
-#     user = models.OneToOneField(
-#         settings.AUTH_USER_MODEL,
-#         on_delete=models.CASCADE,
-#         related_name='wishlist'
-#     )
-#
-#     def __str__(self):
-#         return f"Wishlist for {self.user.email}"
-#
-#
-# class WishlistItem(UUIDModel, TimeStampedModel):
-#     wishlist = models.ForeignKey(
-#         Wishlist,
-#         on_delete=models.CASCADE,
-#         related_name='items'
-#     )
-#     product = models.ForeignKey(
-#         Product,
-#         on_delete=models.CASCADE,
-#         related_name='wishlist_items'
-#     )
-#     added_at = models.DateTimeField(auto_now_add=True)
-#
-#     class Meta:
-#         unique_together = ('wishlist', 'product')
-#
-#     def __str__(self):
-#         return f"{self.product.name} in wishlist of {self.wishlist.user.email}"
+from django.db import models
+from django.conf import settings
+from core.models import UUIDModel, TimeStampedModel
+from products.models import Product
+
+
+class Wishlist(UUIDModel, TimeStampedModel):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='wishlist'
+    )
+
+    def __str__(self):
+        return f"Wishlist for {self.user.email}"
+
+
+class WishlistItem(UUIDModel, TimeStampedModel):
+    wishlist = models.ForeignKey(
+        Wishlist,
+        on_delete=models.CASCADE,
+        related_name='items'
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='wishlist_items'
+    )
+
+    class Meta:
+        unique_together = ('wishlist', 'product')
+
+    def __str__(self):
+        return f"{self.product.name} in wishlist of {self.wishlist.user.email}"
