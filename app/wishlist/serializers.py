b3fc2e37604a9db46a85ca0dbf7ff003e3de9879
RalIlieva <rilieva8820@gmail.com>
2024-10-31 22:16:30 +0200

    Add wishlist serializers

--- app/wishlist/serializers.py
+++ app/wishlist/serializers.py
@@ -1,22 +1,26 @@
-# from rest_framework import serializers
-# from .models import Wishlist, WishlistItem
-# from products.serializers import ProductMiniSerializer
-#
-#
-# class WishlistItemSerializer(serializers.ModelSerializer):
-#     product = ProductMiniSerializer(read_only=True)
-#     product_uuid = serializers.UUIDField(write_only=True)
-#
-#     class Meta:
-#         model = WishlistItem
-#         fields = ['uuid', 'product', 'product_uuid', 'added_at']
-#         read_only_fields = ['uuid', 'product', 'added_at']
-#
-#
-# class WishlistSerializer(serializers.ModelSerializer):
-#     items = WishlistItemSerializer(many=True, read_only=True)
-#
-#     class Meta:
-#         model = Wishlist
-#         fields = ['uuid', 'user', 'items', 'created', 'modified']
-#         read_only_fields = ['uuid', 'user', 'created', 'modified']
+from rest_framework import serializers
+from .models import Wishlist, WishlistItem
+from products.serializers import ProductMiniSerializer
+
+
+class WishlistItemSerializer(serializers.ModelSerializer):
+    product = ProductMiniSerializer(read_only=True)
+    product_uuid = serializers.UUIDField(write_only=True)
+    in_stock = serializers.SerializerMethodField()
+
+    class Meta:
+        model = WishlistItem
+        fields = ['uuid', 'product', 'product_uuid', 'created', 'in_stock']
+        read_only_fields = ['uuid', 'product', 'created', 'in_stock']
+
+    def get_in_stock(self, obj):
+        return obj.product.stock > 0
+
+
+class WishlistSerializer(serializers.ModelSerializer):
+    items = WishlistItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Wishlist
+        fields = ['uuid', 'user', 'items', 'created', 'modified']
+        read_only_fields = ['uuid', 'user', 'created', 'modified']
