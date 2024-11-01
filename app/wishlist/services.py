ef3fa3b1dcecec7afbed39ef54cf55c458959c2c
RalIlieva <rilieva8820@gmail.com>
2024-11-01 16:05:45 +0200

    Refactor wishlist setrvices

--- app/wishlist/services.py
+++ app/wishlist/services.py
@@ -7,7 +7,8 @@ from rest_framework.exceptions import ValidationError, NotFound
 from products.models import Product
 from core.exceptions import (
     ProductAlreadyInWishlistException,
-    ProductNotInWishlistException
+    ProductNotInWishlistException,
+    InsufficientStockError
 )
 from .models import Wishlist, WishlistItem
 
@@ -38,15 +39,27 @@ def remove_product_from_wishlist(user, product_uuid):
     Remove a product from the user's wishlist.
     """
     wishlist = get_or_create_wishlist(user)
-    try:
-        product = Product.objects.get(uuid=product_uuid)
-    except Product.DoesNotExist:
-        raise ProductNotInWishlistException()
-        # raise NotFound("Product not found.")
 
-    wishlist_item = wishlist.items.filter(product=product).first()
-    if wishlist_item:
-        wishlist_item.delete()
+    # Attempt to find the wishlist item that matches the product UUID
+    wishlist_item = wishlist.items.filter(product__uuid=product_uuid).first()
+
+    # If the wishlist item is not found, raise a custom exception
+    if not wishlist_item:
+        raise ProductNotInWishlistException()
+
+    # If found, delete the wishlist item
+    wishlist_item.delete()
+
+    # wishlist = get_or_create_wishlist(user)
+    # try:
+    #     product = Product.objects.get(uuid=product_uuid)
+    # except Product.DoesNotExist:
+    #     raise ProductNotInWishlistException()
+    #     # raise NotFound("Product not found.")
+    #
+    # wishlist_item = wishlist.items.filter(product=product).first()
+    # if wishlist_item:
+    #     wishlist_item.delete()
 
 
 def move_wishlist_item_to_cart(user, product_uuid):
@@ -58,7 +71,8 @@ def move_wishlist_item_to_cart(user, product_uuid):
     product = get_object_or_404(Product, uuid=product_uuid)
 
     if product.stock <= 0:
-        raise ValidationError("This product is currently out of stock.")
+        raise InsufficientStockError()
+        # raise ValidationError("This product is currently out of stock.")
 
     remove_product_from_wishlist(user, product_uuid)
     add_item_to_cart(user, product.id, quantity=1)
