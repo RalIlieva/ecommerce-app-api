c7afbdecfeb1cd0db0eb9b9f69d435dbd6c1e0ca
RalIlieva <rilieva8820@gmail.com>
2024-11-01 14:09:46 +0200

    Register wishlist models in admin

--- app/wishlist/admin.py
+++ app/wishlist/admin.py
@@ -1,3 +1,31 @@
-# from django.contrib import admin
+"""
+Register wishlist models.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import Wishlist, WishlistItem
+
+# admin.site.register(Wishlist)
+
+
+class WishlistItemInline(admin.TabularInline):
+    model = WishlistItem
+    extra = 0  # Remove empty fields for new WishlistItems in the admin panel
+
+
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'uuid', 'user_email', 'created', 'modified'
+    ]
+    list_filter = [
+        'created', 'modified'
+    ]
+    search_fields = ['user__email', 'id']
+    ordering = ['created']
+    inlines = [WishlistItemInline]
+
+    def user_email(self, obj):
+        return obj.user.email
+
+    user_email.short_description = 'User Email'
