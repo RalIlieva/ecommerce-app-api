9517f8f8441cc746208bd8318e6fbc43b80d39f5
RalIlieva <rilieva8820@gmail.com>
2024-11-04 23:21:30 +0200

    Add celery.py in app.py

---
+++ app/app/celery.py
@@ -0,0 +1,23 @@
+# app/celery.py
+
+# import os
+# from celery import Celery
+# # from celery.schedules import crontab
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+#
+# app = Celery('app')
+#
+# # Load task modules from all registered Django app configs.
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+#
+# # Celery will automatically discover tasks in Django apps.
+# app.autodiscover_tasks()
+
+# # Optional: Celery beat configuration (periodic tasks)
+# app.conf.beat_schedule = {
+#     'send-notifications': {
+#         'task': 'app.tasks.send_notification',
+#         'schedule': crontab(minute=0, hour=0),  # Example: Run daily at midnight
+#     },
+# }
