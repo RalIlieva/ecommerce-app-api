34a13a5c60c4833ccc60b34b9969935bc16349e7
RalIlieva <rilieva8820@gmail.com>
2024-11-05 22:34:11 +0200

    Notification signals and apps - to register signals

--- app/notifications/signals.py
+++ app/notifications/signals.py
@@ -1,3 +1,18 @@
+# # order/signals.py
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from order.models import Order
+# from notifications.tasks import send_order_confirmation_email
+#
+#
+# @receiver(post_save, sender=Order)
+# def send_order_confirmation(sender, instance, created, **kwargs):
+#     if created:
+#         # Send email confirmation using Celery task
+#         send_order_confirmation_email.delay(instance.id, instance.user.email)
+
+
+# Intial version
 # from django.db.models.signals import post_save
 # from django.dispatch import receiver
 # from order.models import Order
