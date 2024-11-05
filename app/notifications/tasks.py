ded06202f98e514b53661ef7eef6fc34aebd14d3
RalIlieva <rilieva8820@gmail.com>
2024-11-05 14:16:49 +0200

    Add notifications tasks

--- app/notifications/tasks.py
+++ app/notifications/tasks.py
@@ -1,5 +1,30 @@
 # from celery import shared_task
 # from django.core.mail import send_mail
+# from notifications.models import Notification
+# from django.conf import settings
+#
+#
+# @shared_task
+# def send_email_notification(notification_id):
+#     """
+#     Task to send an email notification.
+#     """
+#     try:
+#         notification = Notification.objects.get(pk=notification_id)
+#         send_mail(
+#             notification.subject,
+#             notification.message,
+#             settings.DEFAULT_FROM_EMAIL,
+#             [notification.user.email],
+#         )
+#         notification.sent = True
+#         notification.save()
+#     except Notification.DoesNotExist:
+#         pass
+
+
+# from celery import shared_task
+# from django.core.mail import send_mail
 #
 #
 # @shared_task
