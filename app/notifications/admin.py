4792975c5530f8be2263a47e422769570a392bdf
RalIlieva <rilieva8820@gmail.com>
2024-11-06 23:44:30 +0200

    Register notifications models in admin.py

--- app/notifications/admin.py
+++ app/notifications/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = [
+        'user', 'notification_type', 'subject', 'status', 'created', 'modified'
+    ]
+    list_filter = ['notification_type', 'status', 'created']
+    search_fields = ['user__email', 'subject']
+    ordering = ['-created']
