e7783d91aab60035571d9303a49715092844c9f6
RalIlieva <rilieva8820@gmail.com>
2024-11-06 22:55:45 +0200

    Register notifications urls in app urls and add namespace notifications

--- app/app/urls.py
+++ app/app/urls.py
@@ -32,4 +32,5 @@ urlpatterns = [
     path('cart/', include('cart.urls', namespace='cart')),
     path('checkout/', include('checkout.urls', namespace='checkout')),
     path('wishlist/', include('wishlist.urls', namespace='wishlist')),
+    path('notifications/', include('notifications.urls', namespace='notifications'))
 ]
--- app/notifications/urls.py
+++ app/notifications/urls.py
@@ -3,6 +3,8 @@
 from django.urls import path
 from .views import NotificationListView, NotificationDetailView
 
+app_name = 'notifications'
+
 urlpatterns = [
     path('notifications/', NotificationListView.as_view(), name='notification-list'),
     path('notifications/<uuid:pk>/', NotificationDetailView.as_view(), name='notification-detail'),
