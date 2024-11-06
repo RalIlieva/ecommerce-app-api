d1ad80330887892e1af1179ef73ae154fd0b7f12
RalIlieva <rilieva8820@gmail.com>
2024-11-06 23:37:14 +0200

    Clean up notifications app code

--- app/notifications/signals.py
+++ app/notifications/signals.py
@@ -4,7 +4,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from order.models import Order
 from notifications.services import handle_order_creation_notification
-# from notifications.tasks import send_order_confirmation_email
 
 
 @receiver(post_save, sender=Order)
@@ -12,6 +11,3 @@ def send_order_confirmation(sender, instance, created, **kwargs):
     if created:
         # Call a service to handle the notification logic
         handle_order_creation_notification(instance)
-
-        # # Send email confirmation using Celery task
-        # send_order_confirmation_email.delay(instance.id, instance.user.email)
