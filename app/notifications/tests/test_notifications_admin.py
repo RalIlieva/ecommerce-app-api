789c0c1ee096bc95fec8505f0bac84018f7831fd
RalIlieva <rilieva8820@gmail.com>
2024-11-07 16:29:10 +0200

    Add doc strings for test_notifications_admin

--- app/notifications/tests/test_notifications_admin.py
+++ app/notifications/tests/test_notifications_admin.py
@@ -7,8 +7,16 @@ User = get_user_model()
 
 
 class NotificationAdminTest(TestCase):
+    """
+    Test cases for the Notification model in the Django Admin interface.
+    """
 
     def setUp(self):
+        """
+        Set up user and notification instance for admin tests.
+        Creates an admin user and logs them in. It also creates a notification
+        instance to verify its availability & access in the Django Admin panel.
+        """
         self.user = User.objects.create_user(
             email='admin@example.com',
             password='password123',
@@ -24,7 +32,11 @@ class NotificationAdminTest(TestCase):
         )
 
     def test_notification_admin_page(self):
-        """Test that notification is accessible in the admin panel."""
+        """
+        Test that notification is accessible in the admin panel.
+        Verifies that the notification changelist page in the Django Admin
+        interface is accessible and contains the expected notification.
+        """
         url = reverse('admin:notifications_notification_changelist')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
@@ -33,6 +45,8 @@ class NotificationAdminTest(TestCase):
     def test_notification_detail_admin_page(self):
         """
         Test that notification detail page is accessible in the admin panel.
+        Verifies that a detailed view of a specific notification is accessible
+        and displays the correct subject in the Django Admin interface.
         """
         url = reverse(
             'admin:notifications_notification_change',
