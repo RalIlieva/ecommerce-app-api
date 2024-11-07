84a27634e16fd7c207b9cbacd96ba0e7152bfbaf
RalIlieva <rilieva8820@gmail.com>
2024-11-07 13:42:37 +0200

    Add test for notification tasks - email failure

--- app/notifications/tests/test_notifications_tasks.py
+++ app/notifications/tests/test_notifications_tasks.py
@@ -43,25 +43,18 @@ class NotificationTaskTest(TestCase):
         self.notification.refresh_from_db()
         self.assertTrue(self.notification.status)
 
+    @patch('notifications.tasks.send_mail', side_effect=Exception('SMTP server error'))
+    def test_send_email_failure(self, mock_send_mail):
+        """
+        Test email sending failure to ensure it's handled properly.
+        This test will patch the `send_mail` function to raise an exception.
+        """
+        # Call the send_order_confirmation_email task
+        send_order_confirmation_email(self.notification.uuid)
+#
+        # Refresh the notification from the database to get the updated status
+        self.notification.refresh_from_db()
 
-# class NotificationTasksFailTest(TestCase):
-#
-#     def setUp(self):
-#         self.user = User.objects.create_user(
-#             email='user@example.com', password='password123'
-#         )
-#         order_uuid = uuid4()
-#         self.order_uuid = order_uuid
-#
-#     @patch('notifications.tasks.send_mail', side_effect=Exception('SMTP server error'))
-#     def test_send_email_failure(self, mock_send_mail):
-#         """
-#         Test email sending failure to ensure it's handled properly.
-#         This test will patch the `send_mail` function to raise an exception.
-#         """
-#         # Call the send_order_confirmation_email task
-#         send_order_confirmation_email(self.order_uuid, self.user.email)
-#
-#         # Check that the notification has been created with a 'False' status
-#         notification = Notification.objects.get(user=self.user, subject=f"Order Confirmation #{self.order_uuid}")
-#         self.assertFalse(notification.status)  # Email sending failed, so the status should be False
+        # Check that the notification status is still False (email was not sent successfully)
+        # Email sending failed, so the status should be False
+        self.assertFalse(self.notification.status)
