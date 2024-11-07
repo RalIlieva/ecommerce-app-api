23f56dd16dc9473f84f5f68f5008ca134df4af58
RalIlieva <rilieva8820@gmail.com>
2024-11-07 14:53:06 +0200

    Add doc string to tasks

--- app/notifications/tasks.py
+++ app/notifications/tasks.py
@@ -13,6 +13,21 @@ logger = logging.getLogger(__name__)
 def send_order_confirmation_email(notification_uuid):
     """
     Task to send an order confirmation email.
+    The email is based on the details in the `Notification` model.
+
+    Workflow:
+        - Retrieve the `Notification` object using the provided UUID.
+        - Send an email using Django's `send_mail` utility.
+        - Update the `status` of `Notification` to `True` if the email is sent.
+
+    Exceptions:
+        - Logs error if the `Notification` instance does not exist.
+        - Logs error for any other exceptions that occur during email sending.
+
+    Notes:
+        - Uses Celery for asynchronous email sending, allowing order creation
+          process to remain non-blocking.
+        - Emails are sent using the SMTP configuration in Django settings.
     """
     try:
         # Get the notification by UUID
