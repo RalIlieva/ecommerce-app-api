103871fb10249fffb3e523b170316b0c397e85cc
RalIlieva <rilieva8820@gmail.com>
2024-11-09 20:25:00 +0200

    Fix flake8 for order urls

--- app/order/urls.py
+++ app/order/urls.py
@@ -20,11 +20,26 @@ urlpatterns = [
     path('orders/', OrderListView.as_view(), name='order-list'),
     path('orders/create/', OrderCreateView.as_view(), name='order-create'),
     path('<uuid:order_uuid>/', OrderDetailView.as_view(), name='order-detail'),
-    path('orders/<uuid:order_uuid>/cancel/', OrderCancelView.as_view(), name='order-cancel'),
+    path(
+        'orders/<uuid:order_uuid>/cancel/', OrderCancelView.as_view(),
+        name='order-cancel'
+    ),
 
     # Admin-facing views
-    path('orders/admin/<uuid:order_uuid>/', AdminOrderDetailView.as_view(), name='admin-order-detail'),
-    path('orders/admin/', AdminOrderListView.as_view(), name='admin-order-list'),
-    # path('admin/orders/', AdminOrderListView.as_view(), name='admin-order-list'),
-    # path('admin/orders/<uuid:order_uuid>/', AdminOrderDetailView.as_view(), name='admin-order-detail'),
+    path(
+        'orders/admin/<uuid:order_uuid>/', AdminOrderDetailView.as_view(),
+        name='admin-order-detail'
+    ),
+    path(
+        'orders/admin/', AdminOrderListView.as_view(),
+        name='admin-order-list'
+    ),
+    # path(
+    # 'admin/orders/', AdminOrderListView.as_view(),
+    # name='admin-order-list'
+    # ),
+    # path(
+    # 'admin/orders/<uuid:order_uuid>/', AdminOrderDetailView.as_view(),
+    # name='admin-order-detail'
+    # ),
 ]
