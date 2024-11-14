d248cf2d9207b7632b8052e426ff68119e78ac3c
RalIlieva <rilieva8820@gmail.com>
2024-11-14 20:36:22 +0200

    Add custom permission for IsVendor in core

---
+++ app/core/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+
+
+class IsVendor(permissions.BasePermission):
+    """Custom permission for vendors."""
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.groups.filter(name='vendor').exists())
