9e6592f4b5675fd585c03a5f47ad4787f35ef46a
RalIlieva <rilieva8820@gmail.com>
2024-11-15 19:29:45 +0200

    Delete vendors/urls

--- app/vendor/urls.py
+++
@@ -1,8 +0,0 @@
-# # vendor/urls.py
-# from django.urls import path, include
-#
-# app_name = 'vendor'
-#
-# urlpatterns = [
-#     path('', include(('vendor.urls.products_urls', 'vendor_products'), namespace='products')),
-# ]
