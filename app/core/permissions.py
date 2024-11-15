a17773c39c9575c549cdd04b9289fa6a6a5822c7
RalIlieva <rilieva8820@gmail.com>
2024-11-15 15:44:10 +0200

    Add vendor-product views and urls

--- app/core/permissions.py
+++ app/core/permissions.py
@@ -8,3 +8,7 @@ class IsVendor(permissions.BasePermission):
         return bool(request.user and request.user.groups.filter(
             name='vendor'
         ).exists())
+
+    def has_object_permission(self, request, view, obj):
+        # Vendors can only manage their own products
+        return obj.vendor == request.user
