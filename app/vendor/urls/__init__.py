b14f51670472aad1f8ae0927b6a647a905a0a458
RalIlieva <rilieva8820@gmail.com>
2024-11-15 19:28:34 +0200

    Change namespaces for vendor urls in init

--- app/vendor/urls/__init__.py
+++ app/vendor/urls/__init__.py
@@ -6,7 +6,7 @@ app_name = 'vendor'
 # Include submodules explicitly
 urlpatterns = [
     path('products/', include('vendor.urls.products_urls', namespace='products')),
-    path('products/', include('vendor.urls.image_urls', namespace='products')),
-    path('categories/', include('vendor.urls.category_urls', namespace='products')),
-    path('tags/', include('vendor.urls.tags_urls', namespace='products')),
+    path('products/', include('vendor.urls.image_urls', namespace='images')),
+    path('categories/', include('vendor.urls.category_urls', namespace='categories')),
+    path('tags/', include('vendor.urls.tags_urls', namespace='tags')),
 ]
