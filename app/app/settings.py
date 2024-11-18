52ce96ae1bab097971897132fe715edb7b4de51c
RalIlieva <rilieva8820@gmail.com>
2024-11-18 22:47:16 +0200

    Add test e-mail for the vendor in the settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -309,3 +309,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
+# Test email for the vendor
+VENDOR_EMAIL = 'vendor@example.com'
