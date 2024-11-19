5a2aec2cf2087b5694ea8b3f9cb694125a052ef1
RalIlieva <rilieva8820@gmail.com>
2024-11-19 18:20:39 +0200

    Register urls in dashboard_urls

---
+++ app/vendor/urls/dashboard_urls.py
@@ -0,0 +1,16 @@
+# vendor/urls/dashboard_urls.py
+
+from django.urls import path
+from vendor.views.dashboard.vendor_dashboard_overview import VendorDashboardView
+from vendor.views.payment.vendor_payment_views import VendorPaymentListView
+from vendor.views.wishlist.vendor_wishlist_views import VendorWishlistView
+from vendor.views.cart.vendor_wishlist_views import VendorCartInfoView
+
+app_name = 'vendor_dashboard'
+
+urlpatterns = [
+    path('dashboard/', VendorDashboardView.as_view(), name='dashboard'),
+    path('payments/', VendorPaymentListView.as_view(), name='payment-list'),
+    path('wishlist/', VendorWishlistView.as_view(), name='wishlist-list'),
+    path('cart/', VendorCartInfoView.as_view(), name='cart-list'),
+]
