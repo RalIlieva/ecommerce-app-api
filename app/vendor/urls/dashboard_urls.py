798ed7dc23ecc457ab2276b73a03db357668d102
RalIlieva <rilieva8820@gmail.com>
2024-11-20 15:43:09 +0200

    Fix flake8

--- app/vendor/urls/dashboard_urls.py
+++ app/vendor/urls/dashboard_urls.py
@@ -1,7 +1,8 @@
 # vendor/urls/dashboard_urls.py
 
 from django.urls import path
-from vendor.views.dashboard.vendor_dashboard_overview import VendorDashboardView
+from vendor.views.dashboard.vendor_dashboard_overview import \
+    VendorDashboardView
 from vendor.views.dashboard.vendor_payment_views import VendorPaymentListView
 from vendor.views.dashboard.vendor_wishlist_views import VendorWishlistView
 from vendor.views.dashboard.vendor_cart_views import VendorCartInfoView
