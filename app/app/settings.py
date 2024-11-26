b44e0bf3cea972c8560b47d62cd92d6837382905
RalIlieva <rilieva8820@gmail.com>
2024-11-26 15:01:34 +0200

    Fix djoser resend password confirmation

--- app/app/settings.py
+++ app/app/settings.py
@@ -69,8 +69,8 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        # 'DIRS': [],
+        # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
