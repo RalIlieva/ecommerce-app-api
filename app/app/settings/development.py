57864a468bad1b5c72a9642c41244abd5e5564d6
RalIlieva <rilieva8820@gmail.com>
2024-12-03 21:52:17 +0200

    Add development settings

---
+++ app/app/settings/development.py
@@ -0,0 +1,23 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+# # Additional development-specific settings
+# INSTALLED_APPS += [
+#     'debug_toolbar',
+#     # ... other development apps
+# ]
+#
+# MIDDLEWARE = [
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+#     *MIDDLEWARE,
+# ]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
+# # Override email backend for development
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
