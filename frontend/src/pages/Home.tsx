af3267cc05aa5f79310e46d117aeadae735b1a71
RalIlieva <rilieva8820@gmail.com>
2024-12-10 18:53:07 +0200

    Add pages and home.tsx

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+// src/pages/Home.tsx
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the E-commerce App</h1>
+    </div>
+  );
+};
+
+export default Home;
