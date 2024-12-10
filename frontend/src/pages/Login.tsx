a7b2f863356c5572b5a469f33e15871bd719dabd
RalIlieva <rilieva8820@gmail.com>
2024-12-10 18:54:23 +0200

    Add login.tsx

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,13 @@
+// src/pages/Login.tsx
+import React from 'react';
+
+const Login: React.FC = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {/* Add your login form here */}
+    </div>
+  );
+};
+
+export default Login;
