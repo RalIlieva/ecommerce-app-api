b4b447d36da42bdfdc39e401e1be81ef7e5d9638
RalIlieva <rilieva8820@gmail.com>
2024-12-11 21:33:41 +0200

    Modify vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 5173,        // Specify the port Vite should use
+    strictPort: true,  // Prevent fallback to another port if 5173 is in use
+  },
 })
