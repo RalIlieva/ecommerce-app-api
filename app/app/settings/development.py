023983fbff4220f567775f81af117102f632b083
RalIlieva <rilieva8820@gmail.com>
2024-12-15 17:31:24 +0200

    Update settings - development.py - to include app and frontend in allowed hosts

--- app/app/settings/development.py
+++ app/app/settings/development.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'app', 'frontend']
 
 # # Additional development-specific settings
 # INSTALLED_APPS += [
