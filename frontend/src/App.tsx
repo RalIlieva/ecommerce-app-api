66138f63e0671eb146340eff8d813f50d27c1c31
RalIlieva <rilieva8820@gmail.com>
2024-12-19 16:12:08 +0200

    Update the product detail url in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,7 +21,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
-        <Route path="/products/:uuid/:slug" element={<ProductDetail />} />
+        <Route path="/products/products/:uuid/:slug" element={<ProductDetail />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
 
