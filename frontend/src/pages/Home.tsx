79820d6f9c7de20ef09a1ad8bce9eea8728622a7
RalIlieva <rilieva8820@gmail.com>
2024-12-20 13:53:16 +0200

    Clean up Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -30,22 +30,3 @@ const Home: React.FC = () => {
 };
 
 export default Home;
-
-// // src/pages/Home.tsx
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-//
-// const Home: React.FC = () => {
-//   return (
-//     <div>
-//       <h1>Welcome to the E-commerce App</h1>
-//       <p>Explore our range of products.</p>
-//       <ul>
-//         <li><Link to="/products">View Products</Link></li>
-//         <li><Link to="/login">Login</Link></li>
-//       </ul>
-//     </div>
-//   );
-// };
-//
-// export default Home;
