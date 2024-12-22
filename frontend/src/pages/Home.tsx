e1cb40984e0ebf7a85d4182e5c1964c2810a9a49
RalIlieva <rilieva8820@gmail.com>
2024-12-22 15:26:35 +0200

    Clean up code

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -10,21 +10,6 @@ const Home: React.FC = () => {
     <div>
       <h1>Welcome to the E-commerce App</h1>
       <p>Explore our range of products.</p>
-{/*       <ul> */}
-{/*         <li><Link to="/products">View Products</Link></li> */}
-{/*         {!user && ( */}
-{/*           <> */}
-{/*             <li><Link to="/register">Register</Link></li> */}
-{/*             <li><Link to="/login">Login</Link></li> */}
-{/*           </> */}
-{/*         )} */}
-{/*         {user && ( */}
-{/*           <> */}
-{/*             <li><Link to="/profile">Profile</Link></li> */}
-{/*             <li><button onClick={logout}>Logout</button></li> */}
-{/*           </> */}
-{/*         )} */}
-{/*       </ul> */}
     </div>
   );
 };
