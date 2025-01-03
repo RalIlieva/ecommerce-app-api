b84dd63cc92c980b4938acf25e71c7ea53fd7d37
RalIlieva <rilieva8820@gmail.com>
2025-01-03 16:13:28 +0200

    Fix flake8

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -96,7 +96,11 @@ class CustomerProfileSerializer(serializers.ModelSerializer):
         """Handle nested updates for user."""
         user_data = validated_data.pop('user', {})
         if user_data:
-            user_serializer = CustomUserSerializer(instance.user, data=user_data, partial=True)
+            user_serializer = CustomUserSerializer(
+                instance.user,
+                data=user_data,
+                partial=True
+            )
             if user_serializer.is_valid():
                 user_serializer.save()
             else:
