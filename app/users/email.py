d9efb003016e65e5bc6cb1ce08cfc5397857af6a
RalIlieva <rilieva8820@gmail.com>
2025-01-14 16:14:00 +0200

    Update email.py

--- app/users/email.py
+++ app/users/email.py
@@ -8,6 +8,7 @@ from django.utils.html import strip_tags
 class CustomPasswordResetEmail(PasswordResetEmail):
     template_name = 'users/email/password_reset.html'
     subject_template_name = 'users/email/password_reset_subject.txt'
+    text_template_name = 'users/email/password_reset.txt'
 
     def send(self, to, *args, **kwargs):
         context = self.get_context_data()
@@ -15,7 +16,8 @@ class CustomPasswordResetEmail(PasswordResetEmail):
             self.subject_template_name, context
         ).strip()
         html_content = render_to_string(self.template_name, context)
-        text_content = strip_tags(html_content)
+        # text_content = strip_tags(html_content)
+        text_content = render_to_string(self.text_template_name, context)
 
         email = EmailMultiAlternatives(
             subject=subject,
