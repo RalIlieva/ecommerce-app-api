e446ef9af5dbaa3d36d099de5e824b24ad76713b
RalIlieva <rilieva8820@gmail.com>
2025-01-17 12:58:35 +0200

    Clean up code

--- frontend/src/components/ChangePassword.tsx
+++ frontend/src/components/ChangePassword.tsx
@@ -36,7 +36,7 @@ const ChangePassword: React.FC = () => {
     setSuccess('Password changed successfully.');
     setError(null);
 
-    // Redirect to profile using the user's UUID
+    // Redirect to profile using the profile's UUID
       setTimeout(() => {
         if (user?.profile_uuid) {
           navigate(`/profile/${user.profile_uuid}`);
@@ -50,17 +50,6 @@ const ChangePassword: React.FC = () => {
       setError(message);
     }
   };
-
-//     // Redirect to profile after a short delay
-//     setTimeout(() => {
-//       navigate('/profile');
-//     }, 3000);
-//   } catch (err: any) {
-//     console.error(err); // Log the error for debugging
-//     const message = err.response?.data?.error || 'An error occurred.';
-//     setError(message);
-//   }
-// };
 
   return (
     <div className="container mt-5">
