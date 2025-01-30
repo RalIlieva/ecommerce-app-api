097fe6092669d61441b7158445b1f45c994aa316
RalIlieva <rilieva8820@gmail.com>
2025-01-30 13:43:52 +0200

    Refactor types.ts

--- frontend/src/api/products.ts
+++ frontend/src/api/products.ts
@@ -2,6 +2,7 @@
 import api from '../api';
 import { Tag } from './tags';
 import { Category } from './categories';
+import { Review } from '../types';
 
 export interface Product {
   uuid: string;
@@ -15,16 +16,16 @@ export interface Product {
   // Add other fields as necessary
 }
 
-export interface Review {
-  uuid: string;
-  rating: number;
-  comment: string;
-  created: string;
-  user: {
-    email: string;
-    // Add other user fields if necessary
-  };
-}
+// export interface Review {
+//   uuid: string;
+//   rating: number;
+//   comment: string;
+//   created: string;
+//   user: {
+//     email: string;
+//     // Add other user fields if necessary
+//   };
+// }
 
 export async function fetchProducts(params?: Record<string, any>) {
   const response = await api.get('/products/products/', { params });
--- frontend/src/api/reviews.ts
+++ frontend/src/api/reviews.ts
@@ -1,16 +1,17 @@
 // src/api/reviews.ts
 import api from '../api';
+import { Review } from '../types';
 
-export interface Review {
-  uuid: string;
-  rating: number;
-  comment: string;
-  created: string;
-  user: {
-    email: string;
-    // Add other user fields if necessary
-  };
-}
+// export interface Review {
+//   uuid: string;
+//   rating: number;
+//   comment: string;
+//   created: string;
+//   user: {
+//     email: string;
+//     // Add other user fields if necessary
+//   };
+// }
 
 export async function fetchReviewsByProduct(uuid: string, slug: string, params?: Record<string, any>) {
   const response = await api.get(`/products/products/${uuid}/${slug}/reviews/`, { params });
