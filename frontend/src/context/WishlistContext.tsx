23677a5b52aac2a273b63b56552aac00e45c10e0
RalIlieva <rilieva8820@gmail.com>
2025-01-31 16:09:13 +0200

    Add WishlistContext.tsx

---
+++ frontend/src/context/WishlistContext.tsx
@@ -0,0 +1,28 @@
+// src/context/WishlistContext.tsx
+
+import React, { createContext, useState, useContext, ReactNode } from 'react';
+
+interface WishlistContextProps {
+  wishlistCount: number;
+  setWishlistCount: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const WishlistContext = createContext<WishlistContextProps | undefined>(undefined);
+
+export const WishlistProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [wishlistCount, setWishlistCount] = useState(0);
+
+  return (
+    <WishlistContext.Provider value={{ wishlistCount, setWishlistCount }}>
+      {children}
+    </WishlistContext.Provider>
+  );
+};
+
+export function useWishlistContext() {
+  const context = useContext(WishlistContext);
+  if (!context) {
+    throw new Error('useWishlistContext must be used within a WishlistProvider');
+  }
+  return context;
+}
