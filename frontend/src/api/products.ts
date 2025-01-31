8e2c18036995fdecf9e232f98ef36e9e7b2a1673
RalIlieva <rilieva8820@gmail.com>
2025-01-31 15:48:56 +0200

    Clean up product.ts

--- frontend/src/api/products.ts
+++ frontend/src/api/products.ts
@@ -15,26 +15,3 @@ export async function fetchProductBySlug(uuid: string, slug: string) {
   const response = await api.get(`/products/products/${uuid}/${slug}/`);
   return response.data;
 }
-
-// export interface Product {
-//   uuid: string;
-//   name: string;
-//   slug: string;
-//   image: string;
-//   price: string;
-//   average_rating?: number;
-//   category: Category;
-//   tags: Tag[];
-//   // Add other fields as necessary
-// }
-
-// export interface Review {
-//   uuid: string;
-//   rating: number;
-//   comment: string;
-//   created: string;
-//   user: {
-//     email: string;
-//     // Add other user fields if necessary
-//   };
-// }
