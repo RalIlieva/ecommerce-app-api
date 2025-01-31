f87b2f1a0b33d728625332db0a7f91e7316b02e9
RalIlieva <rilieva8820@gmail.com>
2025-01-31 16:07:37 +0200

    Add CartContext.tsx

---
+++ frontend/src/context/CartContext.tsx
@@ -0,0 +1,28 @@
+// src/context/CartContext.tsx
+
+import React, { createContext, useState, useContext, ReactNode } from 'react';
+
+interface CartContextProps {
+  cartCount: number;
+  setCartCount: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const CartContext = createContext<CartContextProps | undefined>(undefined);
+
+export const CartProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [cartCount, setCartCount] = useState(0);
+
+  return (
+    <CartContext.Provider value={{ cartCount, setCartCount }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export function useCartContext() {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error('useCartContext must be used within a CartProvider');
+  }
+  return context;
+}
