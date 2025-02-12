b1e13b1f4eb5228917785f26307df3d65fe41738
RalIlieva <rilieva8820@gmail.com>
2025-02-12 13:23:51 +0200

    Clean up CartContext

--- frontend/src/context/CartContext.tsx
+++ frontend/src/context/CartContext.tsx
@@ -28,8 +28,6 @@ export const useCartContext = () => {
   return context;
 };
 
-
-
 // // Version - working wishlist move to cart - badge count
 // import React, { createContext, useContext, useState } from 'react';
 //
@@ -57,32 +55,3 @@ export const useCartContext = () => {
 //   }
 //   return context;
 // };
-
-// Initial version
-// // src/context/CartContext.tsx
-// import React, { createContext, useState, useContext, ReactNode } from 'react';
-//
-// interface CartContextProps {
-//   cartCount: number;
-//   setCartCount: React.Dispatch<React.SetStateAction<number>>;
-// }
-//
-// const CartContext = createContext<CartContextProps | undefined>(undefined);
-//
-// export const CartProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-//   const [cartCount, setCartCount] = useState(0);
-//
-//   return (
-//     <CartContext.Provider value={{ cartCount, setCartCount }}>
-//       {children}
-//     </CartContext.Provider>
-//   );
-// };
-//
-// export function useCartContext() {
-//   const context = useContext(CartContext);
-//   if (!context) {
-//     throw new Error('useCartContext must be used within a CartProvider');
-//   }
-//   return context;
-// }
