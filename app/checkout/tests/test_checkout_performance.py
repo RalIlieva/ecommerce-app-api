d2089e099ff2fce748f89b8be0726f2c7e02ae88
RalIlieva <rilieva8820@gmail.com>
2025-02-24 22:31:21 +0200

    Fix flake8 test checkout performance and checkout flow

--- app/checkout/tests/test_checkout_performance.py
+++ app/checkout/tests/test_checkout_performance.py
@@ -84,7 +84,7 @@ class CheckoutPerformanceTestCase(APITestCase):
 
         # Create a ShippingAddress instance
         shipping_address = ShippingAddress.objects.create(
-            user=self.user,  # Associate the shipping address with the current user
+            user=self.user,
             full_name="Test User",
             address_line_1="789 Oak St",
             address_line_2="Apt 2",
