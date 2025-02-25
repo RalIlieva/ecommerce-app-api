84681b492f4b8a331238477d1322295134012543
RalIlieva <rilieva8820@gmail.com>
2025-02-25 16:55:34 +0200

    Fix checkout performance test

--- app/checkout/tests/test_checkout_performance.py
+++ app/checkout/tests/test_checkout_performance.py
@@ -99,7 +99,7 @@ class CheckoutPerformanceTestCase(APITestCase):
             url, format='json',
             # data={'shipping_address': str(shipping_address.id)}
             data={
-                'shipping_address': {
+                'new_shipping_address': {
                     'full_name': shipping_address.full_name,
                     'address_line_1': shipping_address.address_line_1,
                     'address_line_2': shipping_address.address_line_2,
