424822446996cbbaebd70248c374b0ce3fdba52d
RalIlieva <rilieva8820@gmail.com>
2025-03-04 21:52:48 +0200

    Fix test_create_order_with_multiple_items

--- app/order/tests/test_order_creation.py
+++ app/order/tests/test_order_creation.py
@@ -107,7 +107,16 @@ class OrderCreationTestCase(OrderTestBase):
             'items': [
                 {'product': str(self.product.uuid), 'quantity': 3},
                 {'product': str(product2.uuid), 'quantity': 2}
-            ]
+            ],
+            'shipping_address': {
+                'full_name': "Test User",
+                'address_line_1': "123 Test Street",
+                'address_line_2': "Apt 1",
+                'city': "Test City",
+                'postal_code': "12345",
+                'country': "Testland",
+                'phone_number': "+359883368888"
+            }
         }
         response = self.client.post(ORDER_CREATE_URL, payload, format='json')
 
