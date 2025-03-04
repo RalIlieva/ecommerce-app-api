83ab128027b6b60ae3e899b95c363f18864cd3d8
RalIlieva <rilieva8820@gmail.com>
2025-03-04 22:03:04 +0200

    Fix test_create_order_with_negative_quantity

--- app/order/tests/test_order_creation.py
+++ app/order/tests/test_order_creation.py
@@ -167,7 +167,16 @@ class OrderCreationTestCase(OrderTestBase):
         Test that creating an order with a negative quantity fails with 400.
         """
         payload = {
-            'items': [{'product': str(self.product.uuid), 'quantity': -1}]
+            'items': [{'product': str(self.product.uuid), 'quantity': -1}],
+            'shipping_address': {
+                'full_name': "Test User",
+                'address_line_1': "123 Test Street",
+                'address_line_2': "Apt 1",
+                'city': "Test City",
+                'postal_code': "12345",
+                'country': "Testland",
+                'phone_number': "+359883368888"
+            }
         }
         response = self.client.post(ORDER_CREATE_URL, payload, format='json')
 
