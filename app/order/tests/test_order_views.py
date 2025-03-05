5a35a255e53f3875a17cb0f15a08e8cfc0d15b70
RalIlieva <rilieva8820@gmail.com>
2025-03-05 17:39:27 +0200

    Fix test_create_order_with_valid_data

--- app/order/tests/test_order_views.py
+++ app/order/tests/test_order_views.py
@@ -132,7 +132,16 @@ class OrderCreateViewTests(TestCase):
         self.client.force_authenticate(self.user)
         print("Resolved ORDER_CREATE_URL:", ORDER_CREATE_URL)
         payload = {
-            'items': [{'product': self.product.uuid, 'quantity': 2}]
+            'items': [{'product': self.product.uuid, 'quantity': 2}],
+            'shipping_address': {
+                'full_name': "Test User",
+                'address_line_1': "123 Test Street",
+                'address_line_2': "Apt 1",
+                'city': "Test City",
+                'postal_code': "12345",
+                'country': "Testland",
+                'phone_number': "+359883368888"
+            }
         }
         response = self.client.post(ORDER_CREATE_URL, payload, format='json')
 
