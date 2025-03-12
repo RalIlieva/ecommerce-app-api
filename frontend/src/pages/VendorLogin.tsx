245e89246f293757d4f7c34dc2bebe47224e3155
RalIlieva <rilieva8820@gmail.com>
2025-03-12 14:48:44 +0200

    Update VendorLogin.tsx

--- frontend/src/pages/VendorLogin.tsx
+++ frontend/src/pages/VendorLogin.tsx
@@ -1,3 +1,4 @@
+// src/pages/VendorLogin.tsx
 import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
