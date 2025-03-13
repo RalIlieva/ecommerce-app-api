ac203a486a8e167f72405b8656db3eb4beea01b7
RalIlieva <rilieva8820@gmail.com>
2025-03-13 18:15:19 +0200

    Clean up utils.tsx

--- frontend/src/utils.tsx
+++ frontend/src/utils.tsx
@@ -21,26 +21,3 @@ export function renderStars(rating: number): JSX.Element[] {
   }
   return stars;
 }
-
-// // src/utils.tsx
-// import React from 'react';
-//
-// /**
-//  * Renders star icons for a given numeric rating, 1-5.
-//  * If rating is 4.6, we round to 5. If 4.4, we round to 4, etc.
-//  */
-// export function renderStars(rating: number): JSX.Element[] {
-//   const stars: JSX.Element[] = [];
-//   // Round if you only support whole-number ratings
-//   // or handle half-stars if you prefer more precision.
-//   const roundedRating = Math.round(rating);
-//
-//   for (let i = 1; i <= 5; i++) {
-//     if (i <= roundedRating) {
-//       stars.push(<i key={i} className="fas fa-star text-warning me-1"></i>);
-//     } else {
-//       stars.push(<i key={i} className="far fa-star text-muted me-1"></i>);
-//     }
-//   }
-//   return stars;
-// }
