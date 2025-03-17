6d87fae2b01bf5c4e4ede88b1fcf06f81462f2de
RalIlieva <rilieva8820@gmail.com>
2025-03-17 21:26:40 +0200

    Format code

--- frontend/src/pages/vendor/VendorLogin.tsx
+++ frontend/src/pages/vendor/VendorLogin.tsx
@@ -82,6 +82,7 @@ const VendorLogin: React.FC = () => {
 
 export default VendorLogin;
 
+
 // // src/pages/VendorLogin.tsx
 // import React, { useState, useContext } from 'react';
 // import { useNavigate } from 'react-router-dom';
