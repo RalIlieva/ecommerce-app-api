444eb75d7b80cae90475b4e1215033f0c0f457f2
RalIlieva <rilieva8820@gmail.com>
2025-03-18 15:53:44 +0200

    Update auth_urls to include vendor token obtain view

--- app/vendor/urls/auth_urls.py
+++ app/vendor/urls/auth_urls.py
@@ -1,9 +1,21 @@
 # vendor/urls/auth_urls.py
+
 from django.urls import path
-from vendor.views.auth import vendor_login
+from vendor.views.auth import VendorTokenObtainPairView
 
 app_name = 'vendor_auth'
 
 urlpatterns = [
-    path('login/', vendor_login, name='vendor-login'),
+    path('login/', VendorTokenObtainPairView.as_view(), name='vendor-login'),
 ]
+
+
+# # vendor/urls/auth_urls.py
+# from django.urls import path
+# from vendor.views.auth import vendor_login
+#
+# app_name = 'vendor_auth'
+#
+# urlpatterns = [
+#     path('login/', vendor_login, name='vendor-login'),
+# ]
