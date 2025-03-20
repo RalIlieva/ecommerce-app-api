05b13bc2f7bc4136932267f689a18206ccdc53c1
RalIlieva <rilieva8820@gmail.com>
2025-03-20 13:05:05 +0200

    Clean up code - serializeres_auth

--- app/vendor/serializers/serializers_auth.py
+++ app/vendor/serializers/serializers_auth.py
@@ -27,39 +27,3 @@ class VendorTokenObtainPairSerializer(TokenObtainPairSerializer):
             "groups": [group.name for group in user.groups.all()]
         }
         return data
-
-# # vendor/serializers/serializers_auth.py
-#
-# from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-# from django.contrib.auth import get_user_model
-# from rest_framework import serializers
-#
-# User = get_user_model()
-#
-#
-# class VendorTokenObtainPairSerializer(TokenObtainPairSerializer):
-#     """
-#     Custom TokenObtainPairSerializer that checks
-#     whether the user is in the 'vendor' group.
-#     """
-#
-#     def validate(self, attrs):
-#         # Let the base SimpleJWT serializer do its job:
-#         data = super().validate(attrs)
-#
-#         # At this point, self.user is the authenticated user
-#         user = self.user
-#
-#         # Check if user is in the vendor group
-#         if not user.groups.filter(name='vendor').exists():
-#             raise serializers.ValidationError(
-#                 detail={"detail": "You are not authorized as a vendor."},
-#             )
-#
-#         # Optionally, add user info to the response
-#         data['user'] = {
-#             "id": str(user.id),
-#             "email": user.email
-#         }
-#
-#         return data
