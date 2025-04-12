762a8b460ae6dc2cfa8f182c8c7e88e819790a33
RalIlieva <rilieva8820@gmail.com>
2025-04-12 20:53:22 +0300

    Document VendorPaymentManagement.tsx

--- frontend/src/pages/vendor/VendorPaymentManagement.tsx
+++ frontend/src/pages/vendor/VendorPaymentManagement.tsx
@@ -1,4 +1,4 @@
-// pages/vendor/VendorCartAggregationManagement.tsx
+// pages/vendor/VendorPaymentManagement.tsx
 
 import React, { useState, useEffect } from 'react';
 import { Container, Table, Alert, Button } from 'react-bootstrap';
