28ed8cabe59d96b38f223d026b5eb703c2afe6ae
RalIlieva <rilieva8820@gmail.com>
2025-04-17 14:36:14 +0300

    Implement pagination component in ProductListCategory.tsx

--- frontend/src/pages/ProductListByCategory.tsx
+++ frontend/src/pages/ProductListByCategory.tsx
@@ -4,6 +4,7 @@ import { useParams, Link } from 'react-router-dom';
 import { fetchProducts, Product } from '../api/products';
 import { fetchCategoryDetail, Category } from '../api/categories';
 import ProductGrid from '../components/ProductGrid';
+import Pagination from '../components/Pagination';
 
 const ProductListByCategory: React.FC = () => {
   const { slug } = useParams<{ slug: string }>();
@@ -80,34 +81,41 @@ const ProductListByCategory: React.FC = () => {
           <ProductGrid products={products} />
 
           {/* Pagination */}
-          {totalPages > 1 && (
-            <div className="d-flex justify-content-center mt-4">
-              <ul className="pagination">
-                <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-                  <button className="page-link" onClick={() => setCurrentPage(currentPage - 1)}>
-                    &laquo;
-                  </button>
-                </li>
-                {Array.from({ length: totalPages }, (_, i) => (
-                  <li
-                    key={i + 1}
-                    className={`page-item ${currentPage === i + 1 ? 'active' : ''}`}
-                  >
-                    <button className="page-link" onClick={() => setCurrentPage(i + 1)}>
-                      {i + 1}
-                    </button>
-                  </li>
-                ))}
-                <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
-                  <button className="page-link" onClick={() => setCurrentPage(currentPage + 1)}>
-                    &raquo;
-                  </button>
-                </li>
-              </ul>
-            </div>
-          )}
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={setCurrentPage}
+          />
         </>
       )}
+{/*           {totalPages > 1 && ( */}
+{/*             <div className="d-flex justify-content-center mt-4"> */}
+{/*               <ul className="pagination"> */}
+{/*                 <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}> */}
+{/*                   <button className="page-link" onClick={() => setCurrentPage(currentPage - 1)}> */}
+{/*                     &laquo; */}
+{/*                   </button> */}
+{/*                 </li> */}
+{/*                 {Array.from({ length: totalPages }, (_, i) => ( */}
+{/*                   <li */}
+{/*                     key={i + 1} */}
+{/*                     className={`page-item ${currentPage === i + 1 ? 'active' : ''}`} */}
+{/*                   > */}
+{/*                     <button className="page-link" onClick={() => setCurrentPage(i + 1)}> */}
+{/*                       {i + 1} */}
+{/*                     </button> */}
+{/*                   </li> */}
+{/*                 ))} */}
+{/*                 <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}> */}
+{/*                   <button className="page-link" onClick={() => setCurrentPage(currentPage + 1)}> */}
+{/*                     &raquo; */}
+{/*                   </button> */}
+{/*                 </li> */}
+{/*               </ul> */}
+{/*             </div> */}
+{/*           )} */}
+{/*         </> */}
+{/*       )} */}
 
       <Link to="/categories" className="btn btn-secondary mt-3">
         Back to Categories
