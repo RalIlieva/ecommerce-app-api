99771f99ae87459d48135592f5ac2d52e5b216d6
RalIlieva <rilieva8820@gmail.com>
2025-04-25 17:40:10 +0300

    Format VendorOrderDetail.tsx

--- frontend/src/pages/vendor/VendorOrderDetails.tsx
+++ frontend/src/pages/vendor/VendorOrderDetails.tsx
@@ -1,3 +1,4 @@
+// src/pages/vendor/VendorOrderDetails.tsx
 import React, { useState, useEffect } from 'react';
 import { Container, Card, Button, Alert, Row, Col, Table, Form } from 'react-bootstrap';
 import { useParams, Link } from 'react-router-dom';
