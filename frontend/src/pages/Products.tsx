1a788d1c2860b437aa9aa47d4b85af5fc14521e3
RalIlieva <rilieva8820@gmail.com>
2025-04-27 20:17:18 +0300

    Stable working version

--- frontend/src/pages/Products.tsx
+++ frontend/src/pages/Products.tsx
@@ -205,7 +205,6 @@ const Products: React.FC = () => {
 
 export default Products;
 
-
 // // src/pages/Products.tsx
 // import React, { useEffect, useState } from 'react';
 // import { useLocation } from 'react-router-dom';
