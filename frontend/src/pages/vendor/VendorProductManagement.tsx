ebb37b93ac0312a33f9f0af9706f783b18c9d8d3
RalIlieva <rilieva8820@gmail.com>
2025-05-06 16:50:42 +0300

    Format code

--- frontend/src/pages/vendor/VendorProductManagement.tsx
+++ frontend/src/pages/vendor/VendorProductManagement.tsx
@@ -309,6 +309,7 @@ const VendorProductManagement: React.FC = () => {
 
 export default VendorProductManagement;
 
+
 // // Initial version - to delete
 // // src/pages/vendor/VendorProductManagement.tsx
 // // Loading spinners& UX polishers
