6b2cb412fd44eb15ee0bb67b61a7d54a018e6d61
RalIlieva <rilieva8820@gmail.com>
2025-05-13 15:38:37 +0300

    Clean up vendor/urls/auth_urls.py

--- app/vendor/urls/auth_urls.py
+++ app/vendor/urls/auth_urls.py
@@ -8,14 +8,3 @@ app_name = 'vendor_auth'
 urlpatterns = [
     path('login/', VendorTokenObtainPairView.as_view(), name='vendor-login'),
 ]
-
-
-# # vendor/urls/auth_urls.py
-# from django.urls import path
-# from vendor.views.auth import vendor_login
-#
-# app_name = 'vendor_auth'
-#
-# urlpatterns = [
-#     path('login/', vendor_login, name='vendor-login'),
-# ]
