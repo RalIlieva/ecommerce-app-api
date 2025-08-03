f1fbabd5c1d7e61ebf18236fc58d0dedaf644f5a
RalIlieva <rilieva8820@gmail.com>
2025-08-03 21:24:50 +0300

    Clean up code

--- frontend/src/tests/VendorCategories.test.tsx
+++ frontend/src/tests/VendorCategories.test.tsx
@@ -1,5 +1,4 @@
 // src/tests/VendorCategories.test.tsx
-
 import React from 'react';
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
