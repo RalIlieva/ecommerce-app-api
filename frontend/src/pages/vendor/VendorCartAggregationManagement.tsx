7ae5f8a7daf424f7fff86eb3f270510819f1d1f5
RalIlieva <rilieva8820@gmail.com>
2025-08-10 18:08:13 +0300

    Clean up code

--- frontend/src/pages/vendor/VendorCartAggregationManagement.tsx
+++ frontend/src/pages/vendor/VendorCartAggregationManagement.tsx
@@ -1,4 +1,5 @@
 // Using Axios in VendorCartAggregationManagement.tsx
+
 import React, { useState, useEffect } from 'react';
 import { Container, Table, Alert, Button } from 'react-bootstrap';
 import api from '../../api';
